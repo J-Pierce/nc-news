a98ea93ef1b43f8bf87fb608249666f3f794a945
J-Pierce <james.pierce@hotmail.co.uk>
2025-04-01 21:03:06 +0100

    topics page

--- src/components/Topics/Topics.jsx
+++ src/components/Topics/Topics.jsx
@@ -1,8 +1,34 @@
+import { Link } from "react-router-dom";
+
 export function Topics() {
+  const topics = [
+    {
+      slug: "coding",
+      description: "Code is love, code is life",
+    },
+    {
+      slug: "football",
+      description: "FOOTIE!",
+    },
+    {
+      slug: "cooking",
+      description: "Hey good looking, what you got cooking?",
+    },
+  ];
+
   return (
-    <section>
+    <section className="Topics">
       <h2>Topics</h2>
-      <p></p>
+      {topics.map((topic) => {
+        return (
+          <section className="Topic">
+            <Link to={`/?topic=${topic.slug}`}>
+              <h3>{topic.slug}</h3>
+            </Link>
+            <p>{topic.description}</p>
+          </section>
+        );
+      })}
     </section>
   );
 }
