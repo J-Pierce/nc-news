085f6347cbefe7e026b24f34043549b0383b475e
J-Pierce <james.pierce@hotmail.co.uk>
2025-04-01 13:59:38 +0100

    article vote buttons

--- src/components/Articles/ArticleVotes.jsx
+++ src/components/Articles/ArticleVotes.jsx
@@ -6,38 +6,82 @@ import { patchVotesByArticleId } from "../endpoints";
 
 export function ArticleVotes({ id, votes }) {
   const [vote, setVote] = useState(votes);
+  const [hasUpVoted, setHasUpVoted] = useState(false);
+  const [hasDownVoted, setHasDownVoted] = useState(false);
 
-  function upClicked() {
-    setVote((vote) => {
-      return vote + 1;
-    });
-    patchVotesByArticleId(id, 1).catch(() => {
+  function upClicked(event) {
+    console.log(hasUpVoted);
+    if (("Up: ", hasUpVoted)) {
+      setHasUpVoted(false);
+      event.target.style.color = "#000000";
       setVote((vote) => {
         return vote - 1;
       });
-    });
-  }
+      patchVotesByArticleId(id, -1).catch(() => {
+        setVote((vote) => {
+          return vote + 1;
+        });
+      });
+    } else {
+      setHasUpVoted(true);
 
-  function downClicked() {
-    setVote((vote) => {
-      return vote - 1;
-    });
-    patchVotesByArticleId(id, -1).catch(() => {
+      event.target.style.color = "#ff0000";
       setVote((vote) => {
         return vote + 1;
       });
-    });
+      patchVotesByArticleId(id, 1).catch(() => {
+        setVote((vote) => {
+          return vote - 1;
+        });
+      });
+    }
+  }
+
+  function downClicked(event) {
+    console.log("Down: ", hasDownVoted);
+    if (hasDownVoted) {
+      setHasDownVoted(false);
+      event.target.style.color = "#000000";
+      setVote((vote) => {
+        return vote + 1;
+      });
+      patchVotesByArticleId(id, 1).catch(() => {
+        setVote((vote) => {
+          return vote - 1;
+        });
+      });
+    } else {
+      setHasDownVoted(true);
+      event.target.style.color = "#ff0000";
+      setVote((vote) => {
+        return vote - 1;
+      });
+      patchVotesByArticleId(id, -1).catch(() => {
+        setVote((vote) => {
+          return vote + 1;
+        });
+      });
+    }
   }
 
   return (
     <section className="Votes">
       <p>Votes: </p>
       <button onClick={upClicked}>
-        <FontAwesomeIcon icon={faThumbsUp} size="lg" />
+        <FontAwesomeIcon
+          className="UpVote"
+          icon={faThumbsUp}
+          size="lg"
+          style={{ color: "#000000" }}
+        />
       </button>
       <p>{vote}</p>
       <button onClick={downClicked}>
-        <FontAwesomeIcon icon={faThumbsDown} size="lg" />
+        <FontAwesomeIcon
+          icon={faThumbsDown}
+          size="lg"
+          style={{ color: "#000000" }}
+        />
       </button>
     </section>
   );
