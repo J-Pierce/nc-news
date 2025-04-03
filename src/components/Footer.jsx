2a383592fd73a83e852aff9857839c04c5d0a2ba
J-Pierce <james.pierce@hotmail.co.uk>
2025-04-03 13:10:35 +0100

    style footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 export function Footer() {
   return (
-    <>
+    <section className="links">
       <a href="https://github.com/J-Pierce/nc-news">Frontend Github</a>
       <a href="https://github.com/J-Pierce/NC-News-DB">Backend Github</a>
       <a href="https://nc-news-b93u.onrender.com/api">Backend API</a>
-    </>
+    </section>
   );
 }
